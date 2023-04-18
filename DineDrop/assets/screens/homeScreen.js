978752a59ff4fe6821245d338ea44ab2500ef61a
majaydreskog <maja.ydreskog.6640@student.uu.se>
2023-04-18 12:51:20 +0200

    ändrat lite

--- DineDrop/assets/screens/homeScreen.js
+++ DineDrop/assets/screens/homeScreen.js
@@ -4,7 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function homeScreen(){
 
     return (<View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up App.js to start working on your app</Text>
         <StatusBar style="auto" />
       </View>)
 }
