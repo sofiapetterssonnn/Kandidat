fc143d18cc4b1b898f7bed9e28d644b303fe1f4e
majaydreskog <maja.ydreskog.6640@student.uu.se>
2023-04-19 12:36:53 +0200

    fixa felmeddelande

--- DineDrop/App.js
+++ DineDrop/App.js
@@ -1,4 +1,5 @@
 
+
 import HomeScreen from './assets/screens/homeScreen.js';
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
@@ -27,3 +28,34 @@ export default function App() {
 }
 
 
+
+
+/*import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './assets/screens/homeScreen.js';
+export default function App() {
+  return (
+  <HomeScreen />
+  );
+}
+}
+
+
+import { StyleSheet, Text, View } from 'react-native';
+import LogInScreen from './assets/screens/logInScreen.js';
+export default function App() {
+  return (
+  <LogInScreen />
+  );
+}
+
+
+
+import { StyleSheet, Text, View } from 'react-native';
+import CreateAccountScreen from './assets/screens/createAccountScreen.js';
+
+export default function App() {
+  return (
+  <CreateAccountScreen />
+  );
+}
+*/
