e7a979a4ad73cc45c664a155f3190b7ec6fcb536
majaydreskog <maja.ydreskog.6640@student.uu.se>
2023-05-24 19:51:14 +0200

    lagt till profilescreen kopplar till databas+test

--- DineDrop/assets/screens/profileScreen.js
+++ DineDrop/assets/screens/profileScreen.js
@@ -1,68 +1,113 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList, Animated } from 'react-native';
-import { useNavigation, useIsFocused } from '@react-navigation/native';
+import React, { useEffect, useState, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Animated, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-import { getAuth } from "firebase/auth";
+import { useNavigation, useIsFocused } from '@react-navigation/native';
+import { getAuth } from 'firebase/auth';
+import { doc, getDocs, getDoc, query, collection, where } from 'firebase/firestore';
+import PublishedPost from '/Users/majaydreskog/Desktop/Kandidat/Kandidat/DineDrop/assets/components/publishedPost.js';
 import { FIRESTORE_DB } from '../../config';
-import { useEffect, useState } from 'react';
-import { getDoc, doc } from 'firebase/firestore';
 
 function SettingsButton(props) {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
   return (
-    <TouchableOpacity
-      onPress={() => {
-        //console.log('I am tapped');
-        navigation.navigate('Settings')
-      }}
-    >
+    <TouchableOpacity onPress={() => navigation.navigate('Settings')}>
       <Ionicons style={styles.button} name="ios-settings-outline" size={30} color="#1B2156" />
     </TouchableOpacity>
   );
 }
 
-
 export default function ProfileScreen() {
-
   const auth = getAuth();
   const user = auth.currentUser;
+  const isFocused = useIsFocused();
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  const isFocused = useIsFocused();
-
+  const [userReviews, setUserReviews] = useState([]);
+  const [activeReviewIndex, setActiveReviewIndex] = useState(0);
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const screenWidth = Dimensions.get('window').width;
 
   useEffect(() => {
     const fetchUser = async () => {
-      const docRef = doc(FIRESTORE_DB, "Users", user.uid);
+
+      const docRef = doc(FIRESTORE_DB, 'Users', user.uid);
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
-
-        setFirstName(docSnap.data().firstname)
-        setLastName(docSnap.data().lastname)
+        setFirstName(docSnap.data().firstname);
+        setLastName(docSnap.data().lastname);
+        console.log(firstName, lastName)
       } else {
-        // docSnap.data() will be undefined in this case
-        console.log("No such document!");
+        console.log('No such document!');
       }
     };
-    if (isFocused) {
-      fetchUser()
-    }
 
-  }, [user.uid, isFocused])
+    const fetchUserReviews = async () => {
+
+      const q = query(collection(FIRESTORE_DB, "Reviews"), where("User", "==", user.uid));
+      const querySnapshot = await getDocs(q);
+      const newReviews = [];
+
+      querySnapshot.forEach((doc) => {
+
+        // doc.data() is never undefined for query doc snapshots
+
+
+        newReviews.push(doc.data())
+
+      },);
+      console.log(newReviews)
+      setUserReviews(newReviews)
+
+    };
+
+    if (isFocused) {
+      fetchUser();
+      fetchUserReviews();
+    }
+  }, [user.uid, isFocused]);
+
+  const handleSwipeLeft = () => {
+    if (activeReviewIndex < userReviews.length - 1) {
+      setActiveReviewIndex(activeReviewIndex + 1);
+    }
+  };
+
+  const handleSwipeRight = () => {
+    if (activeReviewIndex > 0) {
+      setActiveReviewIndex(activeReviewIndex - 1);
+    }
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.profileContainer}>
         <SettingsButton />
         <View style={styles.userInformation}>
-          <Ionicons style={styles.profileIcon} name="person" size={40} color={"#B4D6FF"} />
+          <Ionicons style={styles.profileIcon} name="person" size={40} color={'#B4D6FF'} />
           <View>
             <Text style={styles.usernameText}>{user.displayName}</Text>
             <Text style={styles.nameText}>{firstName} {lastName}</Text>
           </View>
         </View>
       </View>
+      <Animated.FlatList
+        data={userReviews}
+        renderItem={({ item }) => (
+          <PublishedPost text={item.text} username={item.username} />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        pagingEnabled
+        onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: scrollX } } }], { useNativeDriver: false })}
+      />
+      <TouchableOpacity style={styles.swipeLeftButton} onPress={handleSwipeLeft}>
+        <Ionicons name="chevron-back-outline" size={40} color="#1B2156" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.swipeRightButton} onPress={handleSwipeRight}>
+        <Ionicons name="chevron-forward-outline" size={40} color="#1B2156" />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -78,8 +123,7 @@ const styles = StyleSheet.create({
   profileContainer: {
     backgroundColor: '#FFFFFF',
     width: '100%',
-    //opacity: 0.7,
-    marginBottom: 680,
+    marginBottom: 20,
     height: '20%',
   },
   profileIcon: {
@@ -99,15 +143,13 @@ const styles = StyleSheet.create({
 
   },
   nameText: {
-    color: "#B4D6FF",
+    color: '#B4D6FF',
     fontWeight: 'bold',
     fontSize: 25,
-    //backgroundColor: 'black',
     paddingTop: 8,
-    marginLeft: 20
-
-
+    marginLeft: 20,
   },
+
   usernameText: {
     color: "#1B2156",
     fontSize: 15,
@@ -115,10 +157,27 @@ const styles = StyleSheet.create({
     marginLeft: 20
     //backgroundColor: 'red',
   },
+
+
   userInformation: {
     flexDirection: 'row',
     //justifyContent: 'space-between',
     top: 75,
     width: '55%'
+  },
+  reviewList: {
+    flexGrow: 1,
+  },
+  swipeLeftButton: {
+    position: 'absolute',
+    top: '50%',
+    left: 20,
+    transform: [{ translateY: -20 }],
+  },
+  swipeRightButton: {
+    position: 'absolute',
+    top: '50%',
+    right: 20,
+    transform: [{ translateY: -20 }],
   },
 });
